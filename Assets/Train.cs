19de643f608e78f484d0f10bd07628c78ab9846e
mwbullen <mwbullen@mikes-mac-mini.home>
2014-09-23 18:07:34 -0400

    Train moves along tracks

---
+++ Assets/Train.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Train : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	GameObject GetLeadCar() {
+		return transform.parent.GetChild (0).gameObject;
+	}
+
+	float GetLeadCarSpeed() {
+		GameObject g = GetLeadCar ();
+
+		//return (float)g.GetComponent<WaypointMover>().movementSpeed;
+
+		return 0f;
+		//float x = g.GetComponent (WaypointMover);
+	}
+}
---
+++ Assets/TrainCar.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+
+public var speedCheckInterval : float = .5f;
+private var timeSinceSpeedCheck :float = 0f;
+
+function Start () {
+
+}
+
+function Update () {
+	timeSinceSpeedCheck += Time.deltaTime;
+	
+	if (timeSinceSpeedCheck >= speedCheckInterval && !IsFirstCar) {
+		SyncSpeedtoFirstCar();
+	}
+}
+
+function GetLeadCarSpeed() {
+	var parentT : Transform =  transform.parent.GetChild(0);
+	
+	var wpm =  parentT.gameObject.GetComponent(WaypointMover);
+	
+	return wpm.movementSpeed;
+}
+
+function SyncSpeedtoFirstCar() {
+	var wpm = GetComponent(WaypointMover);
+	
+	wpm.movementSpeed = GetLeadCarSpeed();	
+	
+}
+
+function IsFirstCar() {
+	var parentT : GameObject =  transform.parent.GetChild(0).gameObject;
+
+	return (parentT == gameObject);
+}
